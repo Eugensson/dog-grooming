bd8ad2d87fbf2ae2814a3c6ca0122462c843bb02
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-19 10:39:08 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+
+const Home = () => {
+  return (
+    <main className="relative w-full max-w-360 mx-auto bg-orange-quaternary">
+      <Hero />
+    </main>
+  );
+};
+
+export default Home;
